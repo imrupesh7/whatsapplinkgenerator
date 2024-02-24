d33710b78b04d4291aed81ea2bb89bf1c5f746e7
Rupesh Jha <rupeshjha7796@gmail.com>
2024-02-25 00:29:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function generateLink() {
+  var mobileNumber = document.getElementById("mobileNumber").value;
+  var queryMsg = document.getElementById("queryMsg").value;
+  if (mobileNumber.length === 10 && queryMsg) {
+    var link = "https://wa.me/" + mobileNumber + "?text=" + encodeURIComponent(queryMsg);
+    document.getElementById("whatsLink").innerText = "Generated link: " + link;
+    document.getElementById("whatsLink").style.display = "block";
+  } else {
+    alert("Please enter a 10-digit mobile number and a query message.");
+  }
+}
