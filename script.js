08b67b55f6b943b424e517ab2c07c49d86c5204a
Rupesh Jha <rupeshjha7796@gmail.com>
2024-02-25 00:32:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function generateLink() {
   var queryMsg = document.getElementById("queryMsg").value;
   if (mobileNumber.length === 10 && queryMsg) {
     var link = "https://wa.me/" + mobileNumber + "?text=" + encodeURIComponent(queryMsg);
-    document.getElementById("whatsLink").innerText = "Generated link: " + link;
+    document.getElementById("whatsLink").innerText = "Generated link: " + 91+link;
     document.getElementById("whatsLink").style.display = "block";
   } else {
     alert("Please enter a 10-digit mobile number and a query message.");
